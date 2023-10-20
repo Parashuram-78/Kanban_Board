d2aeb3e493c56485269a24b17da725f49bf94a98
Ankur Shukla <itzankurhere@gmail.com>
2023-10-20 13:01:56 +0530

    Code Explanation and Comments

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -5,36 +5,47 @@ import { GridLoader } from "react-spinners";
 import Navbar from "../navbar/Navbar";
 import styles from "./styles.module.css"
 import GroupBy from "../group/GroupBy";
+
+// Home component
 const Home = () => {
+  // State variables for grouping, ordering, tickets, and users
   const [groupBy, setgroupBy] = useState(sessionStorage.getItem("groupBy") || "status");
   const [orderBy, setOrderBy] = useState(sessionStorage.getItem("orderBy") || "priority");
   const [tickets, settickets] = useState([]);
   const [users, setUsers] = useState(null);
 
+  // useEffect to fetch data from an API or use cached data from sessionStorage
   useEffect(() => {
-    if(!sessionStorage.getItem("users") || !sessionStorage.getItem("tickets")){
+    if (!sessionStorage.getItem("users") || !sessionStorage.getItem("tickets")) {
+      // Fetch data from the API if it's not cached
       axios
-      .get("https://api.quicksell.co/v1/internal/frontend-assignment")
-      .then((response) => {
-        console.log(response);
-        settickets((prevtickets) => (prevtickets = response.data.tickets));
-        setUsers((prevUser) => (prevUser = response.data.users));
-        sessionStorage.setItem("users",JSON.stringify(response.data.users));
-        sessionStorage.setItem("tickets",JSON.stringify(response.data.tickets));
-      })
-      .catch((error) => {
-        alert("There is some technical issue please try later");
-        console.log(error);
-      });
-    }else{
+        .get("https://api.quicksell.co/v1/internal/frontend-assignment")
+        .then((response) => {
+          console.log(response);
+
+          // Set the fetched data in state
+          settickets((prevtickets) => (prevtickets = response.data.tickets));
+          setUsers((prevUser) => (prevUser = response.data.users));
+
+          // Cache the fetched data in sessionStorage
+          sessionStorage.setItem("users", JSON.stringify(response.data.users));
+          sessionStorage.setItem("tickets", JSON.stringify(response.data.tickets));
+        })
+        .catch((error) => {
+          // Handle errors if the API request fails
+          alert("There is some technical issue, please try again later");
+          console.log(error);
+        });
+    } else {
+      // Use cached data from sessionStorage if it exists
       settickets(JSON.parse(sessionStorage.getItem("tickets")));
       setUsers(JSON.parse(sessionStorage.getItem("users")));
     }
-    
   }, []);
 
   return (
     <div className={styles.mainContainer}>
+      {/* Render the Navbar component */}
       <Navbar
         groupBy={groupBy}
         setgroupBy={setgroupBy}
@@ -44,6 +55,7 @@ const Home = () => {
 
       <div className={styles.container}>
         {users && tickets ? (
+          // If users and tickets data are available, render the GroupBy component
           <GroupBy
             tickets={tickets}
             groupBy={groupBy}
@@ -51,6 +63,7 @@ const Home = () => {
             users={users}
           />
         ) : (
+          // If data is not available, display a loading spinner
           <div className={styles.loader}>
             <GridLoader color="#36d7b7" />
           </div>
--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -2,26 +2,36 @@ import React, { useRef } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import styles from "./styles.module.css";
-import listenForOutsideClicks from "../helper/ListenForQutsideClicks";
+import listenForOutsideClicks from "../helper/ListenForQutsideClicks"; // Import a function for listening to outside clicks
 
+// Navbar component
 const Navbar = ({ groupBy, setgroupBy, orderBy, setOrderBy }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  // Function to toggle the menu open/close
   const toggle = (isOpen) => {
     setIsOpen(!isOpen);
   };
 
+  // Function to handle the group by selection change
   const handleGroupBy = (e) => {
     setgroupBy(e.target.value);
-    sessionStorage.setItem('groupBy', e.target.value);
-  };
-  const handleOrderBy = (e) => {
-    setOrderBy(e.target.value);
-    sessionStorage.setItem('orderBy', e.target.value);
+    sessionStorage.setItem('groupBy', e.target.value); // Store the selected grouping in sessionStorage
   };
 
+  // Function to handle the order by selection change
+  const handleOrderBy = (e) => {
+    setOrderBy(e.target.value);
+    sessionStorage.setItem('orderBy', e.target.value); // Store the selected ordering in sessionStorage
+  };
+
+  // Create a reference to the menu element
   const menuRef = useRef(null);
+
+  // State variable to track whether the menu is actively listening for outside clicks
   const [listening, setListening] = useState(false);
+
+  // Use the listenForOutsideClicks function to add an event listener for outside clicks
   useEffect(listenForOutsideClicks(listening, setListening, menuRef, setIsOpen));
 
   return (
@@ -32,7 +42,7 @@ const Navbar = ({ groupBy, setgroupBy, orderBy, setOrderBy }) => {
             <div
               className={styles.selectOption}
               onClick={() => {
-                toggle(isOpen);
+                toggle(isOpen); // Toggle the menu open/close when clicked
               }}
             >
               <div className={styles.selectIcon}></div>
@@ -40,14 +50,13 @@ const Navbar = ({ groupBy, setgroupBy, orderBy, setOrderBy }) => {
               <div className={styles.arrow}></div>
             </div>
 
-            {isOpen ? (
+            {isOpen ? ( // Conditionally render the options when the menu is open
               <div className={styles.optionBox}>
                 <div className={styles.groupBy}>
                   <div>Grouping</div>
                   <select className={styles.groupBySelect} value={groupBy} onChange={handleGroupBy} id="">
                     <option value="status">Status</option>
                     <option value="priority">Priority</option>
-
                     <option value="user">User</option>
                   </select>
                 </div>
